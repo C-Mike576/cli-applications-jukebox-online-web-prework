3aa5b5d26a6695acbf721aaae433cd0f60332a47
Michael Calkins <m.calkins576@gmail.com>
2019-10-26 15:10:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,8 +29,7 @@ def play(songs)
   if songs.include?(input)
     puts "Playing #{input}"
   elsif  songs.include?(songs[input.to_i-1]) 
-    unless ((input.to_i-1) == -1) 
-    puts "Playing #{songs[input.to_i-1]}"
+    unless ((input.to_i-1) == -1) puts "Playing #{songs[input.to_i-1]}"
   else
     puts "Invalid input, please try again"
   end
